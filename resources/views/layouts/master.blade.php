a2d6c8bb86689c947f4eed06d7b825a7a1835857
donkagunila <donkagunila@gmail.com>
2019-12-24 08:28:29 +0300

    enter

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,6 +12,12 @@
 
 	{{-- css --}}
 	{{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"> --}}
+    <link rel="stylesheet" href="{{ asset('assets/libs/flickity/dist/flickity.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/libs/flickity-fade/flickity-fade.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/libs/aos/dist/aos.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/libs/jarallax/dist/jarallax.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/libs/highlightjs/styles/vs2015.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/libs/%40fancyapps/fancybox/dist/jquery.fancybox.min.css') }}">
 	  <link rel="stylesheet" href="{{ asset('assets/fonts/Feather/feather.css') }}">
 
 	<link rel="stylesheet" href="{{ asset('assets/css/main.css')}}">
@@ -31,6 +37,22 @@
 	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" ></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" ></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"  ></script>
+
+	{{-- <script src="assets/libs/jquery/dist/jquery.min.js"></script> --}}
+    <script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/flickity/dist/flickity.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/flickity-fade/flickity-fade.js') }}"></script>
+    <script src="{{ asset('assets/libs/aos/dist/aos.js') }}"></script>
+    <script src="{{ asset('assets/libs/smooth-scroll/dist/smooth-scroll.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/jarallax/dist/jarallax.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/jarallax/dist/jarallax-video.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/jarallax/dist/jarallax-element.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/typed.js/lib/typed.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/countup.js/dist/countUp.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/highlightjs/highlight.pack.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/%40fancyapps/fancybox/dist/jquery.fancybox.min.js') }}"></script>
+
+	<script src="{{ asset('assets/js/theme.min.js') }}"></script>
 
 
 </body>
--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -4,6 +4,208 @@
 
 @section('contents')
 
+  ================================================== -->
+    <section>
+      <div class="flickity-button-white flickity-button-inset" data-flickity='{"imagesLoaded": true, "wrapAround": true, "pageDots": false}'>
+        <div class="w-100">
+          
+          <!-- Image -->
+          <img src="{{ asset('assets/img/covers/cover-2.png') }}" class="img-fluid w-100" alt="...">
+
+          <!-- Shape -->
+          <div class="shape shape-top shape-fluid-x svg-shim text-white">
+            <svg viewBox="0 0 1440 36" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
+            <path d="M1440 0H0V18C0 18 289.423 58 705 18C1121.5 -19.5 1440 18 1440 18V0Z" fill="currentColor"/>
+            </svg>
+          </div>
+
+        </div>
+        <div class="w-100">
+          
+          <!-- Image -->
+          <img src="{{ asset('assets/img/covers/cover-2.png') }}" class="img-fluid w-100" alt="...">
+
+          <!-- Shape -->
+          <div class="shape shape-top shape-fluid-x svg-shim text-white">
+            <svg viewBox="0 0 1440 36" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
+            <path d="M1440 0H0V18C0 18 289.423 58 705 18C1121.5 -19.5 1440 18 1440 18V0Z" fill="currentColor"/>
+            </svg>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+
+
+
+
+
+
+    <!-- BLOG
+    ================================================== -->
+    <section class="py-8 py-md-11 bg-gradient-light border-bottom">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-12 col-md-10 col-lg-8 text-center">
+            
+            <!-- Badge -->
+            <span class="badge badge-pill badge-primary-soft mb-3">
+              <span class="h6 text-uppercase">Our blog</span>
+            </span>
+
+            <!-- Heading -->
+            <h1>
+              Come up to speed with what has been happening.
+            </h1>
+
+            <!-- Text -->
+            <p class="font-size-lg text-muted mb-7 mb-md-9">
+              We share insights on products and services to ensure you find the quality you seek
+            </p>
+
+          </div>
+        </div> <!-- / .row -->
+        <div class="row">
+          <div class="col-12 col-md-6 col-lg-4 d-flex">
+            
+            <!-- Card -->
+            <a class="card text-reset text-decoration-none shadow-light-lg lift lift-lg mb-6 mb-lg-0" href="#!">
+
+              <!-- Image -->
+              <img src="assets/img/photos/photo-7.png" alt="..." class="card-img-top">
+
+              <!-- Body -->
+              <div class="card-body">
+
+                <!-- Heading -->
+                <h3>
+                  Products should appeal to a class of Quality
+                </h3>
+
+                <!-- Text -->
+                <p class="text-muted mb-5">
+                  Cozy vibes are out and this summer you should focus on making customer's feel brave.
+                </p>
+                
+                <!-- Meta -->
+                <div class="row mt-auto pt-5 border-top">
+                  <div class="col">
+                    
+                    <!-- Author -->
+                    <h6 class="text-uppercase text-muted mb-0">
+                      Remi Ongala
+                    </h6>
+
+                  </div>
+                  <div class="col-auto">
+                    
+                    <!-- Date -->
+                    <p class="h6 text-uppercase text-muted mb-0">
+                      <time datetime="2018-05-02">Dec 21</time>
+                    </p>
+
+                  </div>
+                </div> <!-- / .row -->
+                
+              </div>
+            </a>
+
+          </div>
+          <div class="col-12 col-md-6 col-lg-4 d-flex">
+            
+            <!-- Card -->
+            <a class="card text-reset text-decoration-none shadow-light-lg lift lift-lg mb-6 mb-lg-0" href="#!">
+
+              <!-- Image -->
+              <img src="assets/img/photos/photo-7.png" alt="..." class="card-img-top">
+
+              <!-- Body -->
+              <div class="card-body">
+
+                <!-- Heading -->
+                <h3>
+                  Photos should appeal to a sense of adventure
+                </h3>
+
+                <!-- Text -->
+                <p class="text-muted mb-5">
+                  where quality meets deadlines to get a desired product
+                </p>
+                
+                <!-- Meta -->
+                <div class="row mt-auto pt-5 border-top">
+                  <div class="col">
+                    
+                    <!-- Author -->
+                    <h6 class="text-uppercase text-muted mb-0">
+                      Kelvin Kimbiji
+                    </h6>
+
+                  </div>
+                  <div class="col-auto">
+                    
+                    <!-- Date -->
+                    <p class="h6 text-uppercase text-muted mb-0">
+                      <time datetime="2018-05-02">Dec 21</time>
+                    </p>
+
+                  </div>
+                </div> <!-- / .row -->
+                
+              </div>
+            </a>
+
+          </div>
+          <div class="col-12 col-md-6 col-lg-4 d-flex">
+            
+            <!-- Card -->
+            <a class="card text-reset text-decoration-none shadow-light-lg lift lift-lg mb-6 mb-lg-0" href="#!">
+
+              <!-- Image -->
+              <img src="assets/img/photos/photo-7.png" alt="..." class="card-img-top">
+
+              <!-- Body -->
+              <div class="card-body">
+
+                <!-- Heading -->
+                <h3>
+                  Products should appeal to a class of Quality
+                </h3>
+
+                <!-- Text -->
+                <p class="text-muted mb-5">
+                  Cozy vibes are out and this summer you should focus on making customer's feel brave.
+                </p>
+                
+                <!-- Meta -->
+                <div class="row mt-auto pt-5 border-top">
+                  <div class="col">
+                    
+                    <!-- Author -->
+                    <h6 class="text-uppercase text-muted mb-0">
+                      Remi Ongala
+                    </h6>
+
+                  </div>
+                  <div class="col-auto">
+                    
+                    <!-- Date -->
+                    <p class="h6 text-uppercase text-muted mb-0">
+                      <time datetime="2018-05-02">Dec 21</time>
+                    </p>
+
+                  </div>
+                </div> <!-- / .row -->
+                
+              </div>
+            </a>
+
+          </div>
+        </div> <!-- / .row -->
+      </div> <!-- / .container -->
+    </section>
+
 
 
 
